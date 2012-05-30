6f39938ffb98cee1cd0cd8c7e7c8f4d0a474df87
dazz <annejulia@gmail.com>
2012-05-30 10:16:25 +0200

    added nitices

--- modules/boxes/manifests/setupbox.pp
+++ modules/boxes/manifests/setupbox.pp
@@ -1,10 +1,12 @@
 class boxes::setupbox {
 
+    notice("boxes::setupbox install php")
     # include php
 
     # include apache
     # include apache::php
 
+    notice("boxes::setupbox install php::apache")
     # include php::apache
 
     # include mysql
