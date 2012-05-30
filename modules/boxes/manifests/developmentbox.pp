330529508b89555397fe2c5f8386e988564c66c8
dazz <annejulia@gmail.com>
2012-05-30 19:31:54 +0200

    added developmentbox manifestfile and added some tools

---
+++ modules/boxes/manifests/developmentbox.pp
@@ -0,0 +1,13 @@
+class boxes::developmentbox {
+
+    package {'developer_tools':
+        require => Package['mc', 'aptitude', 'vim'],
+        ensure => present
+     }
+
+#    php::module { 'xdebug':
+#        require => Apt::Sources_list['dotdeb-php53'],
+#        notify  => Service['apache'],
+#        source  => true,
+#    }
+}
