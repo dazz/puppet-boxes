91eb0dda60242f41bb7565ebe16becc28a196eea
dazz <annejulia@gmail.com>
2012-05-31 10:15:59 +0200

    added dev packages

--- modules/boxes/manifests/developmentbox.pp
+++ modules/boxes/manifests/developmentbox.pp
@@ -1,5 +1,17 @@
 class boxes::developmentbox {
 
+    # install xdebug
+    include xdebug
+
+    # install sqlite
+    include sqlite
+
+    # install phpunit
+    package{ ["phpunit"]:
+        ensure => "installed",
+        require => Class["apache::php"],
+    }
+
     # untested until comment removed
     package {'developer_tools':
         require => Package['mc', 'aptitude', 'vim'],
