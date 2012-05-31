61d16ee4c76e471355d360bc34ad7dfe5dcc58c0
dazz <annejulia@gmail.com>
2012-05-31 10:15:40 +0200

    removed php module references

--- modules/boxes/manifests/setupbox.pp
+++ modules/boxes/manifests/setupbox.pp
@@ -4,6 +4,21 @@ class boxes::setupbox {
     apache::php
 
     notice("boxes::setupbox install mysql")
-    include mysql
+    include mysql::server
+
+    # TODO: include augeas module
+    # installs augtool
+    # include augeas
+
+    # add user vagrant to group www-data
+    user { "vagrant":
+        groups => "www-data",
+    }
+
+    # install additional php packages
+    package{ ["php5-intl","php5-mysql","phpunit"]:
+        ensure => "installed",
+        require => Class["apache::php"],
+    }
 
 }
