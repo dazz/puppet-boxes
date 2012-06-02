cb45dff8688d45dfa752a22e578afae327e0c811
dazz <annejulia@gmail.com>
2012-06-02 12:51:24 -0400

    added exec_path

--- modules/boxes/manifests/developmentbox.pp
+++ modules/boxes/manifests/developmentbox.pp
@@ -1,5 +1,7 @@
 class boxes::developmentbox {
 
+    Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/", "/home/vagrant/.rvm/bin" ] }
+
     # untested until comment removed
     package {'developer_tools':
         require => Package['mc', 'aptitude', 'vim'],
