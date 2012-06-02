4110884babf163550eb82333db09343e4ec2062e
dazz <annejulia@gmail.com>
2012-06-02 18:09:19 -0400

    included apt-get update

--- modules/boxes/manifests/basebox.pp
+++ modules/boxes/manifests/basebox.pp
@@ -1,3 +1,21 @@
 class boxes::basebox {
-    notice("boxes::basebox would be installed")
+
+  Exec {
+    path => ['/usr/local/bin', '/opt/local/bin', '/usr/bin', '/usr/sbin', '/bin', '/sbin'],
+    logoutput => true,
+  }
+
+  # the update
+  include apt::update
+
+  Package [require => Exec['apt_update']]
+  # Exec["apt_update"] -> Package <| |>
+
+  # your basebox packages here
+  $package_list = ['vim', 'aptitude', 'sudo', 'mc']
+
+  package {$package_list: }
+
+
 }
+
