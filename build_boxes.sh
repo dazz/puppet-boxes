108de21b0cd9ab0aa728f701c3014639e7cd29ff
dazz <annejulia@gmail.com>
2012-06-03 15:23:35 -0400

    just for the proof, two boxes worked

--- build_boxes.sh
+++ build_boxes.sh
@@ -1,17 +1,29 @@
 #! /bin/bash
 
-boxes=(basebox setupbox productionbox stagebox developmentbox testbox)
+boxes=(basebox setupbox)
+# productionbox stagebox developmentbox testbox)
 
-#SET vagrant_info=DEBUG
+set VAGRANT_LOG=DEBUG
 
 boxes_build() {
   for box in ${boxes[@]}
   do
     pushd boxes/$box >/dev/null
+    echo "[$box] vagrant status"
+    vagrant status
+    echo "[$box] vagrant up"
     vagrant up
+    echo "[$box] vagrant status"
+    vagrant status
+    echo "[$box] vagrant halt"
     vagrant halt
+    echo "[$box] vagrant status"
+    vagrant status
+    echo "[$box] vagrant package $box --output $box.box"
     vagrant package $box --output $box.box
+    echo "[$box] vagrant box add $box $box.box"
     vagrant box add $box $box.box
+    echo "[$box] finish"
 
     popd >/dev/null
     # other stuff on $name
@@ -19,18 +31,42 @@ boxes_build() {
 }
 
 boxes_status() {
-
-  for box in ${boxes[@]}
-  do
+  for box in ${boxes[@]}; do
     pushd boxes/$box >/dev/null
 
+    echo "[$box] vagrant status"
     vagrant status
 
     popd >/dev/null
     # other stuff on $name
   done
+}
+
+boxes_destroy() {
+  for box in ${boxes[@]}; do
+    pushd boxes/$box >/dev/null
+
+    echo "[$box] vagrant destroy -f"
+    vagrant status
+    vagrant destroy -f
+
+    popd >/dev/null
+    # other stuff on $name
+  done
+}
+
+boxes_start() {
+  if [ -n $1 ]; then
+    echo "pushd boxes/$1 >/dev/null"
+    echo "vagrant up"
+    echo "popd >/dev/null"
+  else
+    echo "Please chose one of them: $boxes"
+  fi
 
 }
 
+
 boxes_status
+#boxes_start setupbox
 #boxes_build
