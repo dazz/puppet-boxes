a8968cd3dd0bdf54a86073d884a60ca9574d87cd
dazz <annejulia@gmail.com>
2012-06-03 13:59:53 +0200

    added developmentbox

--- modules/boxes/manifests/developmentbox.pp
+++ modules/boxes/manifests/developmentbox.pp
@@ -1,13 +1,8 @@
 class boxes::developmentbox {
 
-    Exec {
-      path => ['/usr/local/bin', '/opt/local/bin', '/usr/bin', '/usr/sbin', '/bin', '/sbin'],
-      logoutput => true,
-    }
-
     # the update
+    Exec { path => ['/usr/local/bin', '/opt/local/bin', '/usr/bin', '/usr/sbin', '/bin', '/sbin'], logoutput => true }
     include apt::update
-
     #Package [require => Exec['apt_update']]
     Exec["apt_update"] -> Package <| |>
 
