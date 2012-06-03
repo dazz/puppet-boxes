ea476f9fbc958948f34edcec0cc5b90355a92a6e
dazz <annejulia@gmail.com>
2012-06-03 14:06:30 +0200

    added apt-get update

--- modules/boxes/manifests/productionbox.pp
+++ modules/boxes/manifests/productionbox.pp
@@ -1,3 +1,10 @@
 class boxes::productionbox {
 
+    # the update
+    Exec { path => ['/usr/local/bin', '/opt/local/bin', '/usr/bin', '/usr/sbin', '/bin', '/sbin'], logoutput => true }
+    include apt::update
+    #Package [require => Exec['apt_update']]
+    Exec["apt_update"] -> Package <| |>
+
+    # your stuff here
 }
