9974e5abfb01bb637cd7942bfdf4158fa8bf520e
lb_dev-an_scheuermann <anne-julia.scheuermann@little-bird.de>
2012-06-04 17:52:12 +0200

    added installations for symfony project

--- modules/boxes/manifests/productionbox.pp
+++ modules/boxes/manifests/productionbox.pp
@@ -7,4 +7,32 @@ class boxes::productionbox {
     Exec["apt_update"] -> Package <| |>
 
     # your stuff here
+
+    $projectname = "symfony"
+    $targetdir = "/var/www"
+    $projectdir = $targetdir/$projectname
+
+    # url of symfony repo (this should not come over git)
+    $downloadurl = "https://github.com/symfony/symfony-standard"
+
+    exec { "download $projectname":
+        command     => "git clone $downloadurl $projectdir",
+        require     => Package["git-core"],
+        creates     => "$projectdir",
+        logoutput   => on_failure,
+    }
+
+    include apache
+    # setup vhost
+    apache::vhost { "$projectname.local":
+      port     => 80,
+      servername => '',
+      #serveraliases => '',
+      docroot  => $projectdir/web,
+      template => 'apache/vhost-default.conf.erb'
+    }
+
+    $timezone='Europe/Berlin'
+
+
 }
--- modules/boxes/manifests/setupbox.pp
+++ modules/boxes/manifests/setupbox.pp
@@ -7,4 +7,24 @@ class boxes::setupbox {
     Exec["apt_update"] -> Package <| |>
 
     # your stuff here
+
+    # installs augtool for modifying config files (not sure if needed)
+    include augeas
+
+    # installs apache and php
+    apache::php
+
+    # additional php-packages
+    package{ ["php5-intl","php5-mysql"]:
+      ensure => "installed",
+      require => Class["apache::php"],
+    }
+
+    # add user vagrant to group www-data
+    user { "vagrant":
+      groups => "www-data",
+    }
+
+    # include mysql server
+    include mysql::server
 }
