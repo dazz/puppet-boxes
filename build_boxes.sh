d48ecc7c1b0f0657e359dcbfaa5dc15040984546
dazz <annejulia@gmail.com>
2012-06-08 15:54:53 +0200

    removed debug output

--- build_boxes.sh
+++ build_boxes.sh
@@ -298,23 +298,9 @@ Options:
     esac
     ;;
   *)
-    echo "Usage: <option>
-Options:
-  - box
-  - boxes
-"
+    echo "Usage: <box|boxes> <options>"
     ;;
   esac
 }
 
-
-echo "\$1: $1"
-echo "\$2: $2"
-echo "\$3: $3"
-echo "\$4: $4"
-
 start $@
-#boxes_status
-#boxes_destroy
-#boxes_start setupbox
-#boxes_build
